f7a92e3d325b971e5b102874b879813f8d00d7a0
sandstonejr <nsteadman11@gmail.com>
2017-09-25 23:06:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,22 +5,26 @@ var chocolateBars = [
   "skittles"
 ]
 
+//add to front
 //DOES NOT ALTER Original array
 function addElementToBeginningOfArray(arr, item) {
   return [item,...arr]
 }
 
+//add to front
 //DOES alter original array
 function destructivelyAddElementToBeginningOfArray(arr, item) {
   arr.unshift(item)
   return arr
 }
 
+//add to back
 //DOES NOT ALTER Original array
 function addElementToEndOfArray(arr, item) {
   return [arr,...item]
 }
 
+//add to back
 //DOES alter original array
 function destructivelyAddElementToEndOfArray(arr, item) {
   arr.push(item)
