829096c6fbc56811d69a041b6a609dfe07ca2bc3
sandstonejr <nsteadman11@gmail.com>
2017-09-25 23:05:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,14 @@ function destructivelyAddElementToBeginningOfArray(arr, item) {
   arr.unshift(item)
   return arr
 }
+
+//DOES NOT ALTER Original array
+function addElementToEndOfArray(arr, item) {
+  return [arr,...item]
+}
+
+//DOES alter original array
+function destructivelyAddElementToEndOfArray(arr, item) {
+  arr.push(item)
+  return arr
+}
